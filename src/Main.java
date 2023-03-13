f461a6e830e9144a45e03d077e9156b6f5a322be
EGOBenCoco <kirillkasanov291@gmail.com>
2023-03-13 20:43:46 +0200

    Init

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ import java.util.Scanner;
         public static void main(String[] args)
         {
             System.out.println("Hello world");
+            System.out.println();
         }
     }
